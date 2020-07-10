b2588d3bc95df03f6796b08b7ff1b5e015deb06e
Mustafa <mustafa.moiz125@gmail.com>
2020-07-10 16:15:15 -0700

    enable below fold sticky footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet"
 import Header from "./header"
 import Footer from "./footer"
 
-import './layout.scss'
+import "./layout.scss"
 
 import favicon16 from "../images/favicon16.png"
 import favicon32 from "../images/favicon32.png"
@@ -12,28 +12,30 @@ import favicon64 from "../images/favicon64.png"
 
 function getFavicon(size, icon) {
   return {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: size.toString() + 'x' + size.toString(),
-    href: `${icon}`
+    rel: "icon",
+    type: "image/png",
+    sizes: size.toString() + "x" + size.toString(),
+    href: `${icon}`,
   }
 }
 
 export default ({ children }) => (
-  <div className='site'>
-    <Helmet
-      link={[
-        getFavicon(16, favicon16),
-        getFavicon(32, favicon32),
-        getFavicon(64, favicon64),
-      ]}
-    >
-      <html className="has-navbar-fixed-top" lang='en'/>
-    </Helmet>
-    <Header />
-    <section className="section has-background-light site-content is-paddingless">
-      <div className="">{children}</div>
-    </section>
-    <Footer/>
-  </div>
+  <>
+    <div className="site">
+      <Helmet
+        link={[
+          getFavicon(16, favicon16),
+          getFavicon(32, favicon32),
+          getFavicon(64, favicon64),
+        ]}
+      >
+        <html className="has-navbar-fixed-top" lang="en" />
+      </Helmet>
+      <Header />
+      <section className="section has-background-light site-content is-paddingless">
+        <div className="">{children}</div>
+      </section>
+    </div>
+    <Footer />
+  </>
 )
--- src/pages/library.js
+++ src/pages/library.js
@@ -11,13 +11,32 @@ export default ({ data }) => {
       <div className="hero">
         <div className="hero-body">
           <div className="columns is-vcentered is-desktop">
+            <div className="column is-4" style={{ padding: "0 5vmin" }}>
+              <div className="content has-text-centered">
+                <h1
+                  className="title"
+                  style={{
+                    borderBottom: "4px solid #48C774",
+                    textAlign: "left",
+                    paddingBottom: ".3em",
+                  }}
+                >
+                  The Health Library
+                </h1>
+                <p className="is-size-5" style={{ textAlign: "left" }}>
+                  Read up on the conditions we treat, the treatments we offer,
+                  and developments in the fields of psychiatry, mental health,
+                  and wellness.
+                </p>
+              </div>
+            </div>
             <div className="column is-8">
               <div className="columns is-multiline is-desktop">
                 {data.allContentfulPost.nodes.map(
                   ({ slug, title, feature }, i) => {
                     return (
                       <div className="column is-4" key={i}>
-                        <Link to={'/' + slug}>
+                        <Link to={"/" + slug}>
                           <div className="card">
                             <div
                               className="card-image"
@@ -42,8 +61,9 @@ export default ({ data }) => {
                                   right: "0",
                                   padding: ".3em",
                                   textAlign: "left",
-                                  background: "linear-gradient(to top, rgb(64, 64, 64, .8), rgb(0,0,0, 0))",
-                                  borderBottom: '3px solid #48C774',
+                                  background:
+                                    "linear-gradient(to top, rgb(64, 64, 64, .8), rgb(0,0,0, 0))",
+                                  borderBottom: "3px solid #48C774",
                                   maxWidth: "70%",
                                 }}
                               >
@@ -58,25 +78,6 @@ export default ({ data }) => {
                     )
                   }
                 )}
-              </div>
-            </div>
-            <div className="column is-4" style={{ padding: "0 5vmin" }}>
-              <div className="content has-text-centered">
-                <h1
-                  className="title"
-                  style={{
-                    borderBottom: "4px solid #48C774",
-                    textAlign: "left",
-                    paddingBottom: ".3em",
-                  }}
-                >
-                  The Health Library
-                </h1>
-                <p className="is-size-5" style={{ textAlign: "left" }}>
-                  Read up on the conditions we treat, the treatments we offer,
-                  and developments in the fields of psychiatry, mental health,
-                  and wellness.
-                </p>
               </div>
             </div>
           </div>
