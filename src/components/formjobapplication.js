7aee78782a31dc98bd28e8632886e1c5b6788cd1
Mowgl33 <mustafa.moiz125@gmail.com>
2020-07-10 20:23:44 -0700

    Update job app form to allow file upload

--- src/components/formjobapplication.js
+++ src/components/formjobapplication.js
@@ -121,7 +121,7 @@ class FormJobApplication extends React.Component {
             style={{ width: "100%" }}
           >
             <label class="file-label">
-              <input class="file-input" type="file" name="resume" onChange={this.onUpload} ref={this.resume} />
+              <input class="file-input" type="file" name="resume" onChange={this.onUpload} ref={this.state.resume} />
               <span class="file-cta">
                 <span class="file-icon">
                   <FontAwesomeIcon icon={faUpload} />
