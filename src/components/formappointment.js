02eec0c86998ef71ed606bf91916c8c8c55d2a1f
Mustafa <mustafa.moiz125@gmail.com>
2020-08-04 13:47:02 -0700

    rearrange appointment form page

--- src/components/formappointment.js
+++ src/components/formappointment.js
@@ -56,137 +56,145 @@ function FormAppointment() {
 			action='/formsuccess/'
 		>
 			<input type='hidden' name='form-name' value='appointmentrequest' />
-			<h1 className='title'>Request an Appointment</h1>
 
-			<div className='columns'>
-				<div className='column'>
-					<BulmaControl name='First Name'>
+			<div class='columns'>
+				<div class='column is-6'>
+					<div className='columns'>
+						<div className='column'>
+							<BulmaControl name='First Name'>
+								<input
+									className='input'
+									type='text'
+									placeholder='John'
+									name='firstname'
+								/>
+							</BulmaControl>
+						</div>
+						<div className='column'>
+							<BulmaControl name='Last Name'>
+								<input
+									className='input'
+									type='text'
+									placeholder='Smith'
+									name='lastname'
+								/>
+							</BulmaControl>
+						</div>
+					</div>
+					<BulmaControl name='Email' hasLeftIcons>
 						<input
 							className='input'
-							type='text'
-							placeholder='John'
-							name='firstname'
-						/>
-					</BulmaControl>
-				</div>
-				<div className='column'>
-					<BulmaControl name='Last Name'>
-						<input
-							className='input'
-							type='text'
-							placeholder='Smith'
-							name='lastname'
-						/>
-					</BulmaControl>
-				</div>
-			</div>
-			<BulmaControl name='Email' hasLeftIcons>
-				<input
-					className='input'
-					type='email'
-					placeholder='johnsmith@gmail.com'
-					name='email'
-				/>{' '}
-				<span className='icon is-small is-left'>
-					<FontAwesomeIcon icon={faEnvelope} />
-				</span>
-			</BulmaControl>
-			<BulmaControl name='Phone' hasLeftIcons>
-				<input
-					className='input'
-					type='phone'
-					placeholder='(123) 456-7890'
-					name='phone'
-				/>{' '}
-				<span className='icon is-small is-left'>
-					<FontAwesomeIcon icon={faPhone} />
-				</span>
-			</BulmaControl>
-			<BulmaControl name='Date' hasLeftIcons>
-				<input
-					className='input'
-					type='date'
-					placeholder='12/05/1993'
-					name='date'
-				/>{' '}
-				<span className='icon is-small is-left'>
-					<FontAwesomeIcon icon={faCalendar} />
-				</span>
-			</BulmaControl>
-			<BulmaControl name='How Did You Hear About Us?' hasLeftIcons>
-				<div className='select is-success is-fullwidth'>
-					<select
-						name='referralsource'
-						value={referralSource}
-						onChange={handleChange}
-					>
-						{refSources.map((source) => {
-							return <option>{source}</option>;
-						})}
-					</select>
-				</div>
-				<span className='icon is-small is-left'>
-					<FontAwesomeIcon icon={faUserFriends} />
-				</span>
-			</BulmaControl>
-			{referralSource === insuranceOp && (
-				<BulmaControl name='Referring Insurer'>
-					<input
-						className='input'
-						type='text'
-						placeholder='Details...'
-						name='referralinsurance'
-					/>
-				</BulmaControl>
-			)}
-			{referralSource === womOp && (
-				<>
-					<BulmaControl name='Word of Mouth Referrer' hasLeftIcons>
-						<input
-							className='input'
-							type='text'
-							placeholder='Name'
-							name='referralwomname'
-						/>
+							type='email'
+							placeholder='johnsmith@gmail.com'
+							name='email'
+						/>{' '}
 						<span className='icon is-small is-left'>
-							<FontAwesomeIcon icon={faUser} />
+							<FontAwesomeIcon icon={faEnvelope} />
 						</span>
 					</BulmaControl>
-					<BulmaControl name='' hasLeftIcons>
+					<BulmaControl name='Phone' hasLeftIcons>
 						<input
 							className='input'
-							type='text'
+							type='phone'
 							placeholder='(123) 456-7890'
-							name='referralwomphone'
-						/>
+							name='phone'
+						/>{' '}
 						<span className='icon is-small is-left'>
 							<FontAwesomeIcon icon={faPhone} />
 						</span>
 					</BulmaControl>
-				</>
-			)}
-			{referralSource === otherOp && (
-				<BulmaControl name='Please Specify' hasLeftIcons>
-					<input
-						className='input'
-						type='text'
-						placeholder='...'
-						name='referralother'
-					/>
-					<span className='icon is-small is-left'>
-						<FontAwesomeIcon icon={faUser} />
-					</span>
+					<BulmaControl name='Date' hasLeftIcons>
+						<input
+							className='input'
+							type='date'
+							placeholder='12/05/1993'
+							name='date'
+						/>{' '}
+						<span className='icon is-small is-left'>
+							<FontAwesomeIcon icon={faCalendar} />
+						</span>
+					</BulmaControl>
+				</div>
+				<div class='column is-6'>
+					<BulmaControl name='How Did You Hear About Us?' hasLeftIcons>
+						<div className='select is-success is-fullwidth'>
+							<select
+								name='referralsource'
+								value={referralSource}
+								onChange={handleChange}
+							>
+								{refSources.map((source) => {
+									return <option>{source}</option>;
+								})}
+							</select>
+						</div>
+						<span className='icon is-small is-left'>
+							<FontAwesomeIcon icon={faUserFriends} />
+						</span>
+					</BulmaControl>
+					{referralSource === insuranceOp && (
+						<BulmaControl name='Referring Insurer' hasLeftIcons>
+							<input
+								className='input'
+								type='text'
+								placeholder='Insurer'
+								name='referralinsurance'
+							/>
+              								<span className='icon is-small is-left'>
+									<FontAwesomeIcon icon={faUser} />
+								</span>
+						</BulmaControl>
+					)}
+					{referralSource === womOp && (
+						<>
+							<BulmaControl name='Word of Mouth Referrer' hasLeftIcons>
+								<input
+									className='input'
+									type='text'
+									placeholder='Name'
+									name='referralwomname'
+								/>
+								<span className='icon is-small is-left'>
+									<FontAwesomeIcon icon={faUser} />
+								</span>
+							</BulmaControl>
+							<BulmaControl name='' hasLeftIcons>
+								<input
+									className='input'
+									type='text'
+									placeholder='(123) 456-7890'
+									name='referralwomphone'
+								/>
+								<span className='icon is-small is-left'>
+									<FontAwesomeIcon icon={faPhone} />
+								</span>
+							</BulmaControl>
+						</>
+					)}
+					{referralSource === otherOp && (
+						<BulmaControl name='Please Specify' hasLeftIcons>
+							<input
+								className='input'
+								type='text'
+								placeholder='...'
+								name='referralother'
+							/>
+							<span className='icon is-small is-left'>
+								<FontAwesomeIcon icon={faUser} />
+							</span>
+						</BulmaControl>
+					)}
+				</div>
+			</div>
+      <BulmaControl>
+					<button
+						className='button is-success is-outlined'
+						type='submit'
+						style={{ width: '100%' }}
+					>
+						Submit
+					</button>
 				</BulmaControl>
-			)}
-			<BulmaControl>
-				<button
-					className='button is-success is-outlined'
-					type='submit'
-					style={{ width: '100%' }}
-				>
-					Submit
-				</button>
-			</BulmaControl>
 		</form>
 	);
 }
--- src/pages/appointments.js
+++ src/pages/appointments.js
@@ -1,46 +1,46 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import FormAppointment from "../components/formappointment"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import FormAppointment from '../components/formappointment';
 
-import styles from "./appointments.module.scss"
+import styles from './appointments.module.scss';
 
 const disclaimer1 =
-  "This appointment request form requires you to provide personal information for the purposes of scheduling your appointment. By completing and transmitting this form, you consent to disclose such information to a Family Psychiatry Counseling and Wellness affiliated representative. Also, general data (excluding patient identifiable information) may be used for internal analysis purposes."
+	'This appointment request form requires you to provide personal information for the purposes of scheduling your appointment. By completing and transmitting this form, you consent to disclose such information to a Family Psychiatry Counseling and Wellness affiliated representative. Also, general data (excluding patient identifiable information) may be used for internal analysis purposes.';
 const disclaimer2 =
-  "\nIf you prefer to request an appointment by telephone, please contact our office directly at (805) 341-3416 during normal business hours."
+	'\nIf you prefer to request an appointment by telephone, please contact our office directly at (805) 341-3416 during normal business hours.';
 
 export default () => {
-  return (
-    <Layout>
-      <SEO title="FPCW - Appointments" />
-      <div className="hero is-fullheight-with-navbar">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns is-desktop">
-              <div className="column is-6">
-                <figure className="image is-4by3">
-                  <img
-                    style={{ objectFit: "cover" }}
-                    alt="West Coast landscape"
-                    src="https://www.totalvoicetech.com/wp-content/uploads/medical-receptionists-productivity.jpg"
-                  />
-                </figure>
-              </div>
-              <div className="column is-3">
-                <FormAppointment />
-              </div>
-              <div className="column is-3" style={{ height: "auto" }}>
-                <div className={styles.disclaimer} style={{ height: "auto" }}>
-                  <p style={{ marginBottom: "3vmin" }}>{disclaimer1}</p>
-                  <p>{disclaimer2}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<SEO title='FPCW - Appointments' />
+
+			<div class='hero is-fullheight-with-navbar'>
+				<div class='hero-body'>
+					<div className='container'>
+						{/* <div className='column is-9'>
+        							<figure className='image is-4by3'>
+        								<img
+        									style={{ objectFit: 'cover' }}
+        									alt='West Coast landscape'
+        									src='https://www.totalvoicetech.com/wp-content/uploads/medical-receptionists-productivity.jpg'
+        								/>
+        							</figure>
+        						</div> */}
+
+						<h1 className='title is-size-1 is-size-3-mobile'>
+							Request an Appointment
+						</h1>
+
+						<FormAppointment />
+            <div className={styles.disclaimer}>
+							<p>{disclaimer1}</p><br/>
+							<p>{disclaimer2}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
+};
