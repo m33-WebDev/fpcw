959436c945f7572e126870b272e5480460dd9f94
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-07-23 12:16:03 -0700

    Added return statement to exported comp.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,88 +1,88 @@
-import React from 'react';
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faEnvelope,
-	faPhone,
-	faCalendar,
-} from '@fortawesome/free-solid-svg-icons';
+  faEnvelope,
+  faPhone,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
 
-import * as styles from './footer.module.scss';
-import './footer.scss';
+import * as styles from "./footer.module.scss";
+import "./footer.scss";
 
 function Divider() {
-	return (
-		<div
-			className='section is-paddingless is-hidden-mobile'
-			style={{ marginLeft: '5vmin', marginRight: '5vmin' }}
-		>
-			<p>·</p>
-		</div>
-	);
+  return (
+    <div
+      className="section is-paddingless is-hidden-mobile"
+      style={{ marginLeft: "5vmin", marginRight: "5vmin" }}
+    >
+      <p>·</p>
+    </div>
+  );
 }
 
 function Footer() {
-	<footer
-		className={'footer has-background-success has-text-dark '}
-	>
-		<div className='container'>
-			<div className='columns is-desktop is-vcentered is-centered'>
-				<div className='column is-3'>
-					<div className='content'>
-						<table
-							className='table is-completely-borderless has-background-success'
-							style={{ background: 'rgb(250, 250, 250)' }}
-						>
-							<tbody>
-								<tr>
-									<td>
-										<FontAwesomeIcon icon={faPhone} />
-									</td>
-									<td>(805) 341-3416</td>
-								</tr>
-								<tr>
-									<td>
-										<FontAwesomeIcon icon={faEnvelope} />
-									</td>
+  return (
+    <footer className={"footer has-background-success has-text-dark "}>
+      <div className="container">
+        <div className="columns is-desktop is-vcentered is-centered">
+          <div className="column is-3">
+            <div className="content">
+              <table
+                className="table is-completely-borderless has-background-success"
+                style={{ background: "rgb(250, 250, 250)" }}
+              >
+                <tbody>
+                  <tr>
+                    <td>
+                      <FontAwesomeIcon icon={faPhone} />
+                    </td>
+                    <td>(805) 341-3416</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </td>
 
-									<td>fpcw@familypsychiatry101.com</td>
-								</tr>
-								<tr>
-									<td>
-										<FontAwesomeIcon icon={faCalendar} />
-									</td>
-									<td>Monday - Friday, 9:00am - 6:00pm</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
+                    <td>fpcw@familypsychiatry101.com</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <FontAwesomeIcon icon={faCalendar} />
+                    </td>
+                    <td>Monday - Friday, 9:00am - 6:00pm</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
 
-				<Divider />
-				<div className='column is-2'>
-					31194 La Baya Drive
-					<br />
-					Suite 202
-					<br />
-					Westlake Village, CA 91362
-				</div>
-				<Divider />
-				<div className='column is-2'>
-					30851 Agoura Road
-					<br />
-					Suite 304
-					<br />
-					Agoura Hills, CA 91301
-				</div>
-				<Divider />
-				<div className='column is-3'>
-					<div className='content'>
-						<p>© 2020 Family Psychiatry Counseling and Wellness.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
+          <Divider />
+          <div className="column is-2">
+            31194 La Baya Drive
+            <br />
+            Suite 202
+            <br />
+            Westlake Village, CA 91362
+          </div>
+          <Divider />
+          <div className="column is-2">
+            30851 Agoura Road
+            <br />
+            Suite 304
+            <br />
+            Agoura Hills, CA 91301
+          </div>
+          <Divider />
+          <div className="column is-3">
+            <div className="content">
+              <p>© 2020 Family Psychiatry Counseling and Wellness.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import Header from './header';
-import Footer from './footer';
+import Header from "./header";
+import Footer from "./footer";
 
-import './layout.scss';
+import "./layout.scss";
 
-import favicon16 from '../images/favicon16.png';
-import favicon32 from '../images/favicon32.png';
-import favicon64 from '../images/favicon64.png';
+import favicon16 from "../images/favicon16.png";
+import favicon32 from "../images/favicon32.png";
+import favicon64 from "../images/favicon64.png";
 
 function getFavicon(size, icon) {
-	return {
-		rel: 'icon',
-		type: 'image/png',
-		sizes: size.toString() + 'x' + size.toString(),
-		href: `${icon}`,
-	};
+  return {
+    rel: "icon",
+    type: "image/png",
+    sizes: size.toString() + "x" + size.toString(),
+    href: `${icon}`,
+  };
 }
 
 function Layout({ children }) {
-	<>
-		<div className='site'>
-			<Helmet
-				link={[
-					getFavicon(16, favicon16),
-					getFavicon(32, favicon32),
-					getFavicon(64, favicon64),
-				]}
-			>
-				<html className='has-navbar-fixed-top' lang='en' />
-			</Helmet>
-			<Header />
-			<section className='section has-background-light site-content is-paddingless'>
-				<div className=''>{children}</div>
-			</section>
-		</div>
-		<Footer />
-	</>
+  return (
+    <>
+      <div className="site">
+        <Helmet
+          link={[
+            getFavicon(16, favicon16),
+            getFavicon(32, favicon32),
+            getFavicon(64, favicon64),
+          ]}
+        >
+          <html className="has-navbar-fixed-top" lang="en" />
+        </Helmet>
+        <Header />
+        <section className="section has-background-light site-content is-paddingless">
+          <div className="">{children}</div>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
