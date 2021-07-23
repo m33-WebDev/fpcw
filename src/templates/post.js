3784ae235186de9f624335c864a95855001fbf87
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-07-23 13:22:06 -0700

    Renamed query.

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,85 +1,94 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Img from "gatsby-image";
 
-import Layout from '../components/layout';
-import Seo from '../components/seo';
-import RichText from '../components/richtext';
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import RichText from "../components/richtext";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
-import * as pageStyle from './post.module.scss';
+import * as pageStyle from "./post.module.scss";
 
 function Post({ data, pageContext }) {
-	const { title, metaTitle, feature, body, metaDescription } = data.contentfulPost;
+  const {
+    title,
+    metaTitle,
+    feature,
+    body,
+    metaDescription,
+  } = data.contentfulPost;
 
-	return (
-		<Layout>
-			<Seo title={metaTitle || title} description={metaDescription ? metaDescription.metaDescription : ''}/>
-			<div className='hero is-fullheight-with-navbar'>
-				<div className='hero-body'>
-					<div className='container'>
-						<div className='columns is-centered is-desktop is-variable is-8'>
-							<div className='column is-6'>
-								<div className={pageStyle.Frontmatter}>
-									<div className='content'>
-										<h1 className='title' style={{ textAlign: 'left' }}>
-											{title}
-										</h1>
-									</div>
-									<Img
-										fluid={feature.fluid}
-										alt='Post feature'
-										style={{ objectFit: 'cover', marginBottom: '5vmin' }}
-									/>
-									<Link
-										to='/library'
-										className='has-text-success is-hidden-mobile'
-									>
-										<FontAwesomeIcon icon={faChevronLeft} />
-										{' Back to library'}
-									</Link>
-								</div>
-							</div>
-							<div className='column is-6'>
-								<div className={pageStyle.body}>
-									{/* <RichText document={body.json} /> */}
-									<p>---</p>
-									<p style={{ fontStyle: 'italic' }}>
-										If you are experiencing symptoms of mental or physical
-										distress and would like to discuss your treatment options,
-										schedule an appointment with one of our providers today by
-										using our <Link to='/appointments/'>online form</Link> or by
-										calling (805) 341-3416 during normal business hours.
-									</p>
-								</div>
-								<div />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
-};
+  return (
+    <Layout>
+      <Seo
+        title={metaTitle || title}
+        description={metaDescription ? metaDescription.metaDescription : ""}
+      />
+      <div className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered is-desktop is-variable is-8">
+              <div className="column is-6">
+                <div className={pageStyle.Frontmatter}>
+                  <div className="content">
+                    <h1 className="title" style={{ textAlign: "left" }}>
+                      {title}
+                    </h1>
+                  </div>
+                  <Img
+                    fluid={feature.fluid}
+                    alt="Post feature"
+                    style={{ objectFit: "cover", marginBottom: "5vmin" }}
+                  />
+                  <Link
+                    to="/library"
+                    className="has-text-success is-hidden-mobile"
+                  >
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                    {" Back to library"}
+                  </Link>
+                </div>
+              </div>
+              <div className="column is-6">
+                <div className={pageStyle.body}>
+                  {/* <RichText document={body.json} /> */}
+                  <p>---</p>
+                  <p style={{ fontStyle: "italic" }}>
+                    If you are experiencing symptoms of mental or physical
+                    distress and would like to discuss your treatment options,
+                    schedule an appointment with one of our providers today by
+                    using our <Link to="/appointments/">online form</Link> or by
+                    calling (805) 341-3416 during normal business hours.
+                  </p>
+                </div>
+                <div />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
 
-export const postQuery = graphql`
-	query($pagePath: String!) {
-		contentfulPost(slug: { eq: $pagePath }) {
-			slug
-			title
-			metaTitle
-			feature {
-				fluid(maxWidth: 800) {
-					...GatsbyContentfulFluid
-				}
-			}
-			metaDescription {
-				metaDescription
-			}
-		}
-	}
+export const query = graphql`
+  query($pagePath: String!) {
+    contentfulPost(slug: { eq: $pagePath }) {
+      slug
+      title
+      metaTitle
+      feature {
+        fluid(maxWidth: 800) {
+          ...GatsbyContentfulFluid
+        }
+      }
+      metaDescription {
+        metaDescription
+      }
+    }
+  }
 `;
 
 export default Post;
