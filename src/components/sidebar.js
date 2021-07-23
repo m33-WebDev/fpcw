7e5089b53ef8fa2c81ec2066588c20c1eedd4b82
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-07-23 12:14:13 -0700

    Remove unused components.

--- src/components/sidebar.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// // import styles from './sidebar.module.scss';
-
-// export default ({children}) => (
-//     <div style={{maxHeight: '80vh'}}>
-//         {children}
-//     </div>
-// )
