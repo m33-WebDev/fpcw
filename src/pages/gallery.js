724f7731b685b7fb7bc07216368c32acf18efcd6
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-07-23 14:48:48 -0700

    Created minimal gallery page.

---
+++ src/pages/gallery.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+function Gallery() {
+  return (
+    <Layout>
+      <div className="container">
+        <h1 class="title has-text-centered" style={{ marginTop: "2vmin" }}>
+          Photo Gallery
+        </h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default Gallery;
