539d3a091524ce5767332386c3b982d22bc2c292
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-07-23 15:07:50 -0700

    Added photos to gallery.
    Pulled photos from Contentful and generated simple two-column layout.

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,14 +1,56 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 
-function Gallery() {
+export const data = graphql`
+  query {
+    gallery: contentfulGallery(title: { eq: "Office and Staff" }) {
+      photos {
+        gatsbyImageData(width: 800)
+        description
+      }
+    }
+  }
+`;
+
+function Title() {
+  return (
+    <h1 class="title has-text-centered" style={{ marginTop: "2vmin" }}>
+      Photo Gallery
+    </h1>
+  );
+}
+
+function Photos({ photos }) {
+  return (
+    <div className="columns is-multiline is-centered">
+      {photos.map((photo) => (
+        <div className="column is-6">
+          <div class="box">
+            <GatsbyImage
+              image={getImage(photo)}
+              alt="Image"
+              height={400}
+              width={400}
+            />
+            <p>{photo.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Gallery({ data }) {
+  const { gallery } = data;
+
   return (
     <Layout>
       <div className="container">
-        <h1 class="title has-text-centered" style={{ marginTop: "2vmin" }}>
-          Photo Gallery
-        </h1>
+        <Title />
+        <Photos photos={gallery.photos} />
       </div>
     </Layout>
   );
