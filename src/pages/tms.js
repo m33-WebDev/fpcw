05ee4c7a7d419abd58f6d5a4f5a7462ecfa77e87
msmoiz <mustafa.moiz125@gmail.com>
2022-03-13 20:48:10 +0000

    Flesh out basic sections with placeholder content.

--- src/pages/tms.js
+++ src/pages/tms.js
@@ -8,8 +8,10 @@ import {
 } from "react-bulma-components";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import FormAppointment from "../components/formappointment";
+import neurostar from "../images/neurostar-logo.webp";
 
-export default function Tms(props) {
+export default function Tms() {
   const navbar = true;
 
   return (
@@ -22,7 +24,7 @@ export default function Tms(props) {
           <Container>
             <Content>
               <Heading size={1}>Transcranial Magnetic Stimulation</Heading>
-              <Heading subtitle={true} size={3}>
+              <Heading subtitle={true} size={3} renderAs="h2">
                 at Family Psychiatry
               </Heading>
             </Content>
@@ -30,20 +32,31 @@ export default function Tms(props) {
         </Hero.Body>
       </Hero>
 
-      {/* Brief description */}
+      {/* What it does */}
       <Hero size="fullheight" hasNavbar={navbar}>
         <Hero.Body>
           <Container>
-            <Columns>
-              <Content>
-                Our passion here at Collaborative Solutions in Psychiatry is to
-                provide individualized care. Dr. Kirmani-Moe is always looking
-                for treatments for mental health that go beyond prescription
-                medications. With that goal in mind, her practice chose to work
-                with Neurostar to bring Transcranial Magnetic Stimulation (TMS)
-                to its patients. TMS offers not only improvement, but actual
-                remission of even the most severe cases of depression.
-              </Content>
+            <Columns vCentered={true}>
+              <Columns.Column textAlign="center">
+                <img src="https://via.placeholder.com/256" />
+              </Columns.Column>
+              <Columns.Column>
+                <Content>
+                  <Heading textColor="success" renderAs="h2">
+                    Cutting-edge treatment for depression
+                  </Heading>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </Content>
+              </Columns.Column>
             </Columns>
           </Container>
         </Hero.Body>
@@ -51,12 +64,102 @@ export default function Tms(props) {
 
       {/* How it works */}
       <Hero size="fullheight" color="success" hasNavbar={navbar}>
-        <Hero.Body>Section 3</Hero.Body>
+        <Hero.Body>
+          <Container>
+            <Columns vCentered={true}>
+              <Columns.Column>
+                <Content>
+                  <Heading renderAs="h2">Non-invasive and effective</Heading>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </Content>
+              </Columns.Column>
+              <Columns.Column textAlign="center">
+                <img src="https://via.placeholder.com/256" />
+              </Columns.Column>
+            </Columns>
+          </Container>
+        </Hero.Body>
+      </Hero>
+
+      {/* Our process */}
+      <Hero size="fullheight" hasNavbar={navbar}>
+        <Hero.Body>
+          <Container>
+            <Columns vCentered={true}>
+              <Columns.Column textAlign="center">
+                <img src="https://via.placeholder.com/256" />
+              </Columns.Column>
+              <Columns.Column>
+                <Content>
+                  <Heading textColor="success" renderAs="h2">
+                    Our process
+                  </Heading>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </Content>
+              </Columns.Column>
+            </Columns>
+          </Container>
+        </Hero.Body>
+      </Hero>
+
+      {/* Our partners */}
+      <Hero size="fullheight" color="success" hasNavbar={navbar}>
+        <Hero.Body>
+          <Container textAlign="center">
+            <Heading renderAs="h2">Our partners</Heading>
+            <a href="https://neurostar.com/" target="_blank">
+              <img src={neurostar} alt="Neurostar Logo" />
+            </a>
+          </Container>
+        </Hero.Body>
       </Hero>
 
       {/* Sign up */}
       <Hero size="fullheight" hasNavbar={navbar}>
-        <Hero.Body>Section 4</Hero.Body>
+        <Hero.Body>
+          <Container>
+            <Columns gap="8">
+              <Columns.Column>
+                <Content>
+                  <Heading renderAs="h2" textColor="success">
+                    Sign up for a free consultation
+                  </Heading>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </Content>
+              </Columns.Column>
+              <Columns.Column>
+                <FormAppointment />
+              </Columns.Column>
+            </Columns>
+          </Container>
+        </Hero.Body>
       </Hero>
     </Layout>
   );
