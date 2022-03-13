59b44f84c49d1ec0d4391abf81c2adc26964ca1b
Mustafa Moiz <msmoiz@amazon.com>
2022-03-13 19:41:09 +0000

    Further work.

--- src/pages/tms.js
+++ src/pages/tms.js
@@ -1,23 +1,61 @@
 import React from "react";
+import {
+  Heading,
+  Hero,
+  Content,
+  Container,
+  Columns,
+} from "react-bulma-components";
 import Layout from "../components/layout";
-import { Hero } from "react-bulma-components";
+import Seo from "../components/seo";
 
 export default function Tms(props) {
+  const navbar = true;
+
   return (
     <Layout>
-      <Hero size="fullheight">
-        <Hero.Body>Section 1</Hero.Body>
+      <Seo title="Transcranial Magnetic Stimulation (TMS)" />
+
+      {/* Splash / intro */}
+      <Hero size="fullheight" color="success" hasNavbar={navbar}>
+        <Hero.Body>
+          <Container>
+            <Content>
+              <Heading size={1}>Transcranial Magnetic Stimulation</Heading>
+              <Heading subtitle={true} size={3}>
+                at Family Psychiatry
+              </Heading>
+            </Content>
+          </Container>
+        </Hero.Body>
       </Hero>
 
-      <Hero size="fullheight">
-        <Hero.Body>Section 2</Hero.Body>
+      {/* Brief description */}
+      <Hero size="fullheight" hasNavbar={navbar}>
+        <Hero.Body>
+          <Container>
+            <Columns>
+              <Content>
+                Our passion here at Collaborative Solutions in Psychiatry is to
+                provide individualized care. Dr. Kirmani-Moe is always looking
+                for treatments for mental health that go beyond prescription
+                medications. With that goal in mind, her practice chose to work
+                with Neurostar to bring Transcranial Magnetic Stimulation (TMS)
+                to its patients. TMS offers not only improvement, but actual
+                remission of even the most severe cases of depression.
+              </Content>
+            </Columns>
+          </Container>
+        </Hero.Body>
       </Hero>
 
-      <Hero size="fullheight">
+      {/* How it works */}
+      <Hero size="fullheight" color="success" hasNavbar={navbar}>
         <Hero.Body>Section 3</Hero.Body>
       </Hero>
 
-      <Hero size="fullheight">
+      {/* Sign up */}
+      <Hero size="fullheight" hasNavbar={navbar}>
         <Hero.Body>Section 4</Hero.Body>
       </Hero>
     </Layout>
