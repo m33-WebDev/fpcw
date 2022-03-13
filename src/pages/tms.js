e51de1b92cbab8ee59dc293d563b501604ac34fe
msmoiz <mustafa.moiz125@gmail.com>
2022-03-13 22:10:19 +0000

    Add nice styling to images.

--- src/pages/tms.js
+++ src/pages/tms.js
@@ -10,6 +10,14 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import FormAppointment from "../components/formappointment";
 import neurostar from "../images/neurostar-logo.webp";
+import styled from "styled-components";
+
+const imageSmilingMan =
+  "https://images.unsplash.com/photo-1520451644838-906a72aa7c86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=850&q=80";
+const imageShakingHands =
+  "https://images.unsplash.com/photo-1522973717924-b10fe4e185cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80";
+const imageVeryHappy =
+  "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
 
 export default function Tms() {
   const navbar = true;
@@ -21,13 +29,24 @@ export default function Tms() {
       {/* Splash / intro */}
       <Hero size="fullheight" color="success" hasNavbar={navbar}>
         <Hero.Body>
-          <Container>
+          <Container style={{ position: "relative" }}>
             <Content>
               <Heading size={1}>Transcranial Magnetic Stimulation</Heading>
               <Heading subtitle={true} size={3} renderAs="h2">
                 at Family Psychiatry
               </Heading>
             </Content>
+            {/* <img
+              src="http://getdrawings.com/img/flower-silhouette-clip-art-12.png"
+              alt="heh"
+              style={{
+                position: "absolute",
+                right: 0,
+                top: "50%",
+                color: "white",
+              }}
+              width="512px"
+            /> */}
           </Container>
         </Hero.Body>
       </Hero>
@@ -38,7 +57,13 @@ export default function Tms() {
           <Container>
             <Columns vCentered={true}>
               <Columns.Column textAlign="center">
-                <img src="https://via.placeholder.com/256" />
+                <FancyImage
+                  src={imageSmilingMan}
+                  width="512px"
+                  height="512px"
+                  radius="30% 70% 70% 30% / 30% 30% 70% 70%"
+                  color="success"
+                />
               </Columns.Column>
               <Columns.Column>
                 <Content>
@@ -83,7 +108,12 @@ export default function Tms() {
                 </Content>
               </Columns.Column>
               <Columns.Column textAlign="center">
-                <img src="https://via.placeholder.com/256" />
+                <FancyImage
+                  src={imageShakingHands}
+                  width="512px"
+                  height="512px"
+                  radius="30% 70% 20% 80% / 70% 30% 70% 30%"
+                />
               </Columns.Column>
             </Columns>
           </Container>
@@ -96,7 +126,13 @@ export default function Tms() {
           <Container>
             <Columns vCentered={true}>
               <Columns.Column textAlign="center">
-                <img src="https://via.placeholder.com/256" />
+                <FancyImage
+                  src={imageVeryHappy}
+                  width="512px"
+                  height="512px"
+                  radius="16% 84% 24% 76% / 70% 15% 85% 30%"
+                  color="success"
+                />
               </Columns.Column>
               <Columns.Column>
                 <Content>
@@ -164,3 +200,11 @@ export default function Tms() {
     </Layout>
   );
 }
+
+const FancyImage = styled.img`
+  padding: 5px;
+  border: dotted 5px;
+  border-radius: ${(props) => props.radius};
+  border-color: ${(props) =>
+    props.color === "success" ? "#48C774" : "#F5F5F5"};
+`;
