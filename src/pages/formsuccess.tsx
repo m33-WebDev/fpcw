7819f4858a40ff5538e5f1e5477cba459ec29c85
Mustafa Moiz <msmoiz@amazon.com>
2022-11-20 22:06:03 -0800

    Add missing return statement to success from head.
    Without the return statement, the head method returned undefined which
    in turn caused the render method to fail for the page.

--- src/pages/formsuccess.tsx
+++ src/pages/formsuccess.tsx
@@ -4,7 +4,9 @@ import { Container, Content, Section } from "react-bulma-components";
 import { Layout, Seo, Typography } from "../components";
 
 export function Head() {
-    <Seo title="FPCW - Success" description="Form submission successful. Please return to the main page." noIndex />;
+    return (
+        <Seo title="FPCW - Success" description="Form submission successful. Please return to the main page." noIndex />
+    );
 }
 
 export default function FormSuccess() {
