e1453abd7c4f38e3fb8e49b6a2f59d2a5f8fc052
msmoiz <mustafa.moiz125@gmail.com>
2023-01-17 02:32:29 +0000

    chore: delete public folder contents and ignore
    The public folder is generated on build and does not need to be source
    controlled. It was originally added to source control for reasons that
    are unknown.

--- public/component---cache-caches-gatsby-plugin-offline-app-shell-js-433b8d6c0539722b239c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfpcw=self.webpackChunkfpcw||[]).push([[306],{1367:function(e,t,n){var u=n(4836);t.__esModule=!0,t.default=void 0;var f=u(n(7867)),r=u(n(7294)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,f.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),l=a;t.default=l}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-433b8d6c0539722b239c.js.map
