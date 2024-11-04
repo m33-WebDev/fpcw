0a5b37f412b26b64c6fc18f7ec8ac46afbd97eff
msmoiz <mustafa.moiz125@gmail.com>
2024-11-03 19:46:26 -0800

    feat: silence sass deprecation warnings

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+                // silence warnings until the following issues are resolved
+                // upstream: https://github.com/jgthms/bulma/issues/3907,
+                // https://github.com/jgthms/bulma/issues/3920
+                quietDeps: true,
+            },
+        },
+    },
 });
